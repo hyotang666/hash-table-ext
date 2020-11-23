88730400b7e21df962794d0d4a27ce481b970049
Shinichi Sato <hyotang.karakoma@gmail.com>
2020-11-23 18:44:24 +0900

    Add: Spec for DOHT.

--- spec/hash-table-ext.lisp
+++ spec/hash-table-ext.lisp
@@ -107,3 +107,89 @@
 
 ;;;; Exceptional-Situations:
 
+(requirements-about DOHT :doc-type function)
+
+;;;; Description:
+
+#+syntax (DOHT ((v-key &optional v-value) hash-form &optional return)
+           &body
+           body)
+; => result
+
+;;;; Arguments and Values:
+
+; v-key := symbol, otherwise signals implementation dependent condition.
+#?(DOHT (("not symbol") (MAKE-HASH-TABLE))) :signals CONDITION
+; Not evaluated.
+#?(doht ((intern "NOT EVALUATED")) (make-hash-table)) :signals condition
+; Bound by each keys of hash-table.
+#?(doht ((k) (pairht '(:a :b) '(1 2)))
+    (princ k))
+:outputs "AB"
+; You can use `NIL` for k when what you interests is only value.
+#?(doht ((nil v) (pairht '(:a :b) '(1 2)))
+    (princ v))
+:outputs "12"
+
+; v-value := symbol, otherwise signals implementation dependent condition.
+#?(DOHT ((KEY "not symbol") (MAKE-HASH-TABLE))) :signals CONDITION
+; Not evaluated
+#?(doht ((key (intern "NOT EVALUATED")) (make-hash-table))) :signals condition
+; Bound by each value of hash-table.
+#?(doht ((k v) (pairht '(:a :b) '(1 2)))
+    (format t "~A~A" k v))
+:outputs "A1B2"
+
+; hash-form := The form which generates hash-table, otherwise signals implementation dependent condition.
+#?(DOHT ((KEY) "not hash-table")) :signals CONDITION
+
+; return := NIL, if specified this form is evaluated at last and return its values.
+#?(doht ((k) (pairht '(:a :b) '(1 2)) (princ :last))
+    (princ k))
+:outputs "ABLAST"
+; In the RETURN form, variable v-key and v-value is visible but NIL.
+#?(doht((k v) (pairht '(:a) '(1)) (list k v))
+    (princ (list k v)))
+=> (NIL NIL)
+,:stream nil
+,:test equal
+
+; body := implicit progn.
+; Declarable.
+#?(doht ((k) (pairht '(:a) '(1)))
+    (declare (type symbol k))
+    (princ k))
+:outputs "A"
+; Returnable.
+#?(doht ((nil v) (pairht '(:a :b :c) '(1 2 3)))
+    (if (evenp v)
+      (return nil)
+      (princ v)))
+:outputs "1"
+; GOable.
+#?(doht ((nil v) (pairht '(:a :b :c) '(1 2 3)))
+    (when (evenp v)
+      (go :end))
+    (princ v)
+    :end)
+:outputs "13"
+
+; result := NIL as default.
+#?(doht ((k) (make-hash-table))
+    (princ k))
+=> NIL
+; If RETURN is specified all values are returned.
+#?(doht ((k v) (make-hash-table) (values k v))
+    (format t "~A~A" k v))
+:values (NIL NIL)
+
+;;;; Affected By:
+; none.
+
+;;;; Side-Effects:
+; none.
+
+;;;; Notes:
+; See [Traversal rules and side effect.](http://clhs.lisp.se/Body/03_f.htm)
+
+;;;; Exceptional-Situations:
